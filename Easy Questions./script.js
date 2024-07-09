b67e80ecf56ecd230c9a2a0c474ac98226949fae
Shahab Uddin Ghori <shahabnisar918@gmail.com>
2024-07-09 23:47:51 +0500

    problem 1 twosum

--- Easy Questions./script.js
+++ Easy Questions./script.js
@@ -61,3 +61,57 @@ var passThePillow = function (n, time) {
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Problem 1. Two Sum
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+*/
+
+console.log("\n\n\t LEET Code Easy Problem 1");
+
+let twoSum = (targetNumber, givenArray) => {
+    let targetNumberIndex = [];
+  for (let i = 0; i < givenArray.length; i++) {
+    for (let j = i + 1; j < givenArray.length; j++) {
+      if (givenArray[i] + givenArray[j] == targetNumber) {
+        targetNumberIndex.push(i, j);
+        return targetNumberIndex
+      }
+    }
+  }
+};
+let result = twoSum(9,[2,7,11,15])
+console.log(result);// output [0,1] 
+
+// similar way 
+/*var twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return []; // Return an empty array if no pair is found
+};
+
+// Example usage:
+let nums = [2, 7, 11, 15];
+let target = 9;
+let result = twoSum(nums, target);
+console.log(result); // Output: [0, 1] (since 2 + 7 == 9)
+*/
