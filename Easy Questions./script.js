42b479001efe5d5b8811701030699eed421b174c
Shahab Uddin Ghori <shahabnisar918@gmail.com>
2024-07-13 00:36:28 +0500

    leet code

--- Easy Questions./script.js
+++ Easy Questions./script.js
@@ -640,3 +640,18 @@ maxProfit = Math.max(5, 4 - 1) = 5
 Toh final maxProfit = 5.
 */
 
+
+//27. Remove Element
+
+console.log("\n\n\t 27. Remove Element");
+const removeElement = (nums, val) => {
+  // Loop in reverse order to avoid skipping elements
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+    }
+  }
+  return nums.length;
+};
+
+console.log(removeElement([0,1,2,2,3,0,4,2], 2));
